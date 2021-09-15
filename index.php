9c17c59576d54a101a81dd017268f09d1626d2b3
M.Emmad Saiq <57301519+EmmadSadiq01@users.noreply.github.com>
2021-09-15 23:22:13 +0500

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>CRUD app</title>
+    <title>CRUD app 1</title>
 
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
 </head>
